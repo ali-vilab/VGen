ab287000c8e77adbd67ce9ca8c58cf4e085313d8
Hangjie Yuan <30721381+JacobYuan7@users.noreply.github.com>
2024-06-16 23:57:09 +0800

    configs

---
+++ configs/instructvideo/eval_generate_videos.sh
@@ -0,0 +1,6 @@
+python inference.py --cfg configs/instructvideo/eval/infer_UNetSD_t2v_webvid_LoRA_webvid_ddim50_in-domain.yaml
+# python inference.py --cfg configs/instructvideo/eval/infer_UNetSD_t2v_webvid_LoRA_webvid_ddim20_in-domain.yaml
+# python inference.py --cfg configs/instructvideo/eval/infer_UNetSD_t2v_webvid_LoRA_ddim20_generalization_non-animals.yaml
+# python inference.py --cfg configs/instructvideo/eval/infer_UNetSD_t2v_webvid_LoRA_ddim20_generalization_new-animals.yaml
+# python inference.py --cfg configs/instructvideo/eval/modelscopet2v_infer_UNetSD_t2v_ddim20_in-domain.yaml
+# python inference.py --cfg configs/instructvideo/eval/modelscopet2v_infer_UNetSD_t2v_ddim20_new-animals.yaml
